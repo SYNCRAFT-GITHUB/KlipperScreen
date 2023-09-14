1bcfa4408b0fa67b5c1064d4282957bc98b8bb5c
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2023-09-14 13:30:46 -0300

    fixed moonraker script  in quick system fixes >X1

--- scripts/fix/moonraker.sh
+++ scripts/fix/moonraker.sh
@@ -12,7 +12,7 @@ if [ -d "$moonraker_dir" ]; then
     sudo rm -r $moonraker_dir
 fi
 
-git clone --quiet -b syncraftx1 https://github.com/SYNCRAFT-GITHUB/moonraker.git
+git clone --quiet https://github.com/SYNCRAFT-GITHUB/moonraker.git
 
 if [ -e "$script_dir" ]; then
     bash $script_dir
