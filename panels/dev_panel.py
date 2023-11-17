6afd4b4a84890e27c155af7be6f7cb532b84abdd
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2023-11-17 17:55:55 -0300

    create panel to test multiple sensors (dev_panel)

---
+++ panels/dev_panel.py
@@ -0,0 +1,122 @@
+import logging
+import random
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, Pango
+
+from ks_includes.KlippyGcodes import KlippyGcodes
+from ks_includes.screen_panel import ScreenPanel
+
+
+def create_panel(*args):
+    return DevPanel(*args)
+
+class DevPanel(ScreenPanel):
+
+    def __init__(self, screen, title):
+
+        super().__init__(screen, title)
+        self.menu = ['dev_panel']
+
+        self.current_extruder = self._printer.get_stat("toolhead", "extruder")
+
+        filament_sensors_list = self._printer.get_filament_sensors()
+        endstops_sensors_list = ["Endstop X", "Endstop Y", "Endstop Z"]
+        probe_sensors_list = ["probe"]
+
+        spool_sensors_grid = Gtk.Grid()
+        self.arrange_grid(spool_sensors_grid)
+
+        endstop_sensors_grid = Gtk.Grid()
+        self.arrange_grid(endstop_sensors_grid)
+
+        probe_sensors_grid = Gtk.Grid()
+        self.arrange_grid(probe_sensors_grid)
+
+        self.limit: int = 5
+
+        self.loop(filament_sensors_list, spool_sensors_grid)
+        self.loop(endstops_sensors_list, endstop_sensors_grid)
+        self.loop(probe_sensors_list, probe_sensors_grid)
+
+        self.content.add(spool_sensors_grid)
+        self.content.add(endstop_sensors_grid)
+        self.content.add(probe_sensors_grid)
+
+    def loop(self, sensor_list, sensor_grid):
+        if len(sensor_list) > 0:
+            for number_index, x in enumerate(sensor_list):
+                if number_index > self.limit:
+                    break
+                name = x[23:].strip()
+                self.labels[x] = {
+                    'label': Gtk.Label(f"\n\t{x.replace('_', ' ').title()}\t\n"),
+                    'box': Gtk.Box()
+                }
+                self.labels[x]['box'].pack_start(self.labels[x]['label'], True, True, 5)
+                self.labels[x]['box'].get_style_context().add_class("filament_sensor")
+                sensor_grid.attach(self.labels[x]['box'], 0, number_index, 1, 1)
+
+    def arrange_grid(self, grid):
+        grid.set_column_spacing(15)
+        grid.set_row_spacing(15)
+        grid.set_halign(Gtk.Align.CENTER)
+        grid.set_valign(Gtk.Align.CENTER)
+
+    def process_update(self, action, data):
+        
+        if random.randint(1, 5) == 1:
+            query_endstops_cmd = "query_endstops"
+            query_probe_cmd = "query_probe"
+            self._screen._ws.klippy.gcode_script(query_endstops_cmd)
+            self._screen._ws.klippy.gcode_script(query_probe_cmd)
+
+        def style_box(label, style, remove: bool):
+            if remove:
+                self.labels[label]['box'].get_style_context().remove_class(style)
+            else:
+                self.labels[label]['box'].get_style_context().add_class(style)
+
+        def update_endstop_label(e, data):
+            label = f"Endstop {e.upper()}"
+            if f'{e}:open' in data:
+                style_box(label, "filament_sensor_detected", remove=True)
+                style_box(label, "filament_sensor_empty", remove=False)
+            if f'{e}:TRIGGERED' in data:
+                style_box(label, "filament_sensor_empty", remove=True)
+                style_box(label, "filament_sensor_detected", remove=False)
+
+        if action == "notify_gcode_response":
+
+            if 'probe: ' in data:
+                if 'probe: open' in data:
+                    style_box("probe", "filament_sensor_empty", remove=True)
+                    style_box("probe", "filament_sensor_detected", remove=False)
+                if 'probe: TRIGGERED' in data:
+                    style_box("probe", "filament_sensor_empty", remove=True)
+                    style_box("probe", "filament_sensor_detected", remove=False)
+
+            update_endstop_label(e="x", data=data)
+            update_endstop_label(e="y", data=data)
+            update_endstop_label(e="z", data=data)
+
+        if action == "notify_busy":
+            return
+        if action != "notify_status_update":
+            return
+
+        for x in self._printer.get_filament_sensors():
+            if x in data:
+                if 'enabled' in data[x]:
+                    self._printer.set_dev_stat(x, "enabled", data[x]['enabled'])
+                if 'filament_detected' in data[x]:
+                    self._printer.set_dev_stat(x, "filament_detected", data[x]['filament_detected'])
+                    if self._printer.get_stat(x, "enabled"):
+                        if data[x]['filament_detected']:
+                            style_box(x, "filament_sensor_empty", remove=True)
+                            style_box(x, "filament_sensor_detected", remove=False)
+                        else:
+                            style_box(x, "filament_sensor_detected", remove=True)
+                            style_box(x, "filament_sensor_empty", remove=False)
+                logging.info(f"{x}: {self._printer.get_stat(x)}")
