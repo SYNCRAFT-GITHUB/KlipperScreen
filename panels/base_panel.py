ed4e987d7f7b507faf0d47c69d5cae8145003c03
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2023-12-18 16:28:21 -0300

    prevent ks from crashing of value does not exist in variables.cfg

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -317,10 +317,13 @@ class BasePanel(ScreenPanel):
             if not os.path.exists('/home/pi/printer_data/config/variables.cfg'):
                 self.titlelbl.set_label(f" ")
                 return
-            nozzle = self._config.variables_value_reveal('nozzle')
-            current_ext = self._config.variables_value_reveal('currentextruder')
-            material_ext0 = self._config.variables_value_reveal('material_ext0')
-            material_ext1 = self._config.variables_value_reveal('material_ext1')
+            try:
+                nozzle = self._config.variables_value_reveal('nozzle')
+                current_ext = self._config.variables_value_reveal('currentextruder')
+                material_ext0 = self._config.variables_value_reveal('material_ext0')
+                material_ext1 = self._config.variables_value_reveal('material_ext1')
+            except:
+                nozzle = current_ext = material_ext0 = material_ext1 = '?'
 
             if current_ext == False:
                 current_ext = _("Error")
