b15213be59c57cd81f81ecc53ce41efa0fec1839
RafaelSwi <rafanswi@protonmail.com>
2024-03-07 17:39:36 -0300

    update

--- panels/nozzle.py
+++ panels/nozzle.py
@@ -57,4 +57,20 @@ class NozzleSelectPanel(ScreenPanel):
             })
 
     def nozzlegcodescript(self, widget, nozzle: str):
+        self._config.replace_nozzle(newvalue=nozzle)
         self._screen._ws.klippy.gcode_script(f"NOZZLE_SET NZ='{nozzle}'")
+
+    def process_update(self, action, data):
+
+        for x in self._printer.get_filament_sensors():
+
+            if x in data:
+                
+                if 'enabled' in data[x]:
+                    self._printer.set_dev_stat(x, "enabled", data[x]['enabled'])
+                if 'filament_detected' in data[x]:
+                    self._printer.set_dev_stat(x, "filament_detected", data[x]['filament_detected'])
+                    if self._printer.get_stat(x, "enabled"):
+                        if not data[x]['filament_detected'] and x == self._config.get_spool_option():
+                            self._config.replace_filament_activity(x, "empty")
+                            self._screen._menu_go_back()
